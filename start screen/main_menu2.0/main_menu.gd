7464e74b80eb801b69a2353f7b44dac5b4ac6bc0
klinkl21 <klinkl@wsc.school.nz>
2024-07-05 11:59:01 +1200

    fixing start menu
    labeling things properlly

--- start screen/main_menu2.0/main_menu.gd
+++ start screen/main_menu2.0/main_menu.gd
@@ -1,15 +1,15 @@
 class_name MainMenu
-
 extends Control
-@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Button as Button 
-@onready var help_button = $MarginContainer/HBoxContainer/VBoxContainer/Button2 as Button 
-@onready var exit_button = $VBoxContainer/Button5 as Button 
-@onready var settings_button = $VBoxContainer2/Button4 as Button 
+
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/start_button as Button 
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/exit_button as Button 
+@onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/options_button as Button 
 @onready var start_level = preload("res://Level/level_main.tscn") as PackedScene
  
+
 func _ready():
-	start_button.button_down.connect(on_start_pressed)
-	exit_button.button_down.connect(on_exit_pressed)
+	handle_connecting_signals() 
 	
 	
 func on_start_pressed() -> void:
@@ -18,4 +18,8 @@ func on_start_pressed() -> void:
 	
 func on_exit_pressed() -> void:
 	get_tree().quit()
+
+func handle_connecting_signals() -> void:
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
 
