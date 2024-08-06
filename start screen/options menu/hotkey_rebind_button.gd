6d709221a85b3c9a11925366462d1422ffd4c2f1
klinkl21 <klinkl@wsc.school.nz>
2024-08-07 10:35:25 +1200

    fix
    16:52 trying to fix action keys

--- start screen/options menu/hotkey_rebind_button.gd
+++ start screen/options menu/hotkey_rebind_button.gd
@@ -20,20 +20,12 @@ func set_action_name() -> void:
 		"move_left":
 			label.text = "move left"
 		"move_Right":
-			label.text = "move Right"
+			label.text = "move right"
 
 
-func set_text_for_key() -> void:
+func set_text_for_key():
 	var action_events = InputMap.action_get_events(action_name)
-	if action_events.size() > 0:
-		var action_event = action_events[0]
-		var action_keycode = OS.get_keycode_string(action_event.physical_keycode)
-	
-		button.text = "%s" % action_keycode
-	
-	
-
-
+	print(action_events)
 
 
 
