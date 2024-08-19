9bfa716f811c3354c726adcc5d38e1a6940320a8
klinkl21 <klinkl@wsc.school.nz>
2024-08-19 14:24:22 +1200

    sound working hopefuly

--- start screen/main_menu/options menu/music/audio_slider_settings.gd
+++ start screen/main_menu/options menu/music/audio_slider_settings.gd
@@ -6,11 +6,34 @@ extends Control
 @onready var h_slider = $HBoxContainer/HSlider as HSlider
 
 
-enum BusNames  {Music}
+#enum BusNames  {Music}
+@export_enum("Music") var bus_name : String
 
-@export var bus_name : BusNames = BusNames.Music
+#@export var bus_name : BusNames = BusNames.Music
 
 var bus_index : int = 1
 
 func _ready():
-	HSlider.value_changed.connect(on_value_changed)
+	h_slider.value_changed.connect(on_value_changed)
+	get_bus_name_by_index()
+	set_name_label_text()
+	set_slider_value()
+
+func set_name_label_text() -> void:
+	audio_name_lbl.text = str(bus_name) + " Volume"
+	
+
+func set_audio_num_label_text() -> void:
+	audio_num_lbl.text = str(h_slider.value * 100)
+
+
+func get_bus_name_by_index() -> void:
+	bus_index = AudioServer.get_bus_index(bus_name)
+
+func set_slider_value() -> void:
+	h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	set_audio_num_label_text()
+
+func on_value_changed(value : float) -> void: 
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	set_audio_num_label_text()
